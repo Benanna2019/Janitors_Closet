95c84e2b309aaa0e417c48dcb8b03499e71fb710
Benanna2019 <bass41992ben@gmail.com>
2020-08-25 10:15:51 -0400

    Created Filter Function

---
+++ filter.js
@@ -0,0 +1,18 @@
+let array = [4, 7, 3, 6, 5, 2, 9, 10, 12];
+
+let filteredArray = array.filter((x) => x % 2 === 0);
+
+// console.log(filteredArray);
+
+function filter(fn, arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(filter((x) => x % 2 === 0, array));
+module.exports = filter;
